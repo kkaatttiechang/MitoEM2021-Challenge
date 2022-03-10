73f6d40d503d108e36a37149579e0173182e01cc
Katie Chang <katiechang@wifi-131-179-48-212.host.ucla.edu>
2022-03-10 10:11:55 -0800

    fixed path and added python var for flexibility

--- connectomics/utils/evaluation/iteration_eval.py
+++ connectomics/utils/evaluation/iteration_eval.py
@@ -22,15 +22,15 @@ def cal_infer(root_dir, model_id):
     """
     If you have enough resources, you can use this function during training. 
     Confirm that this line is open. 
-    https://github.com/Limingxing00/MitoEM2021-Challenge/blob/dddb388a4aab004fa577058b53c39266e304fc03/connectomics/engine/trainer.py#L423
+    https://github.com/Limingxing00/MitoEM2021_Challenge/blob/dddb388a4aab004fa577058b53c39266e304fc03/connectomics/engine/trainer.py#L423
     """
 
-    command = "/opt/conda/bin/python {}scripts/main.py --config-file\
-                {}configs/MitoEM/MitoEM-R-BC.yaml\
+    command = "{} {}/scripts/main.py --config-file\
+                {}/configs/MitoEM/MitoEM-R-BC.yaml\
                 --inference\
                 --do_h5\
                 --checkpoint\
-                {}outputs/dataset_output/checkpoint_{:06d}.pth.tar\
+                {}/outputs/dataset_output/checkpoint_{:06d}.pth.tar\
                 --opts\
                 SYSTEM.ROOTDIR\
                 {}\
@@ -52,17 +52,17 @@ def cal_infer(root_dir, model_id):
                 [0,256,256]\
                 INFERENCE.AUG_NUM\
                 0\
-            ".format(root_dir, root_dir, root_dir, model_id, root_dir)
+            ".format(which_python, root_dir, root_dir, root_dir, model_id, root_dir)
 
     out = subprocess.run(command, shell=True)
     print(command, "\n |-------------| \n", out, "\n |-------------| \n")
 
-    command = "/opt/conda/bin/python {}connectomics/utils/evaluation/evaluate.py \
+    command = "{} {}/connectomics/utils/evaluation/evaluate.py \
                  -gt \
-                 {}/human_val_gt.h5 \
+                 {}/0_human_instance_seg_pred.h5 \
                  -p \
-                 {}outputs/inference_output/{:06d}_out_100_256_256_aug_0_pad_0.h5 \
-             -o {}{:06d}".format(root_dir, root_dir, model_id, root_dir, model_id)
+                 {}/outputs/inference_output/{:06d}_out_100_256_256_aug_0_pad_0.h5 \
+             -o {}{:06d}".format(which_python, root_dir, root_dir, model_id, root_dir, model_id)
 
     out = subprocess.run(command, shell=True)
     print(command, "\n |-------------| \n", out, "\n |-------------| \n")
@@ -73,26 +73,28 @@ if __name__=="__main__":
     """
     Please note to change the gt file!
     My gt file is in:
-    /braindat/lab/limx/MitoEM2021/MitoEM-H/MitoEM-H/human_val_gt.h5
+    /braindat/lab/limx/MitoEM2021/MitoEM-H/MitoEM-H/0_human_instance_seg_pred.h5
     """
     # change the "start_epoch" and "end_epoch"  to infer "root_dir/model"
     start_epoch, end_epoch = 297000, 297000
     step_epoch = 2500
     model_id = range(start_epoch, end_epoch+step_epoch, step_epoch)
 
-    root_dir = "." # WRITE PATH TO FOLDER
+    root_dir = "." # WRITE PATH TO ROOT 
+    global which_python 
+    which_python = "." # WRITE PATH TO PYTHON, i.e. /opt/conda/bin/python
     #"/braindat/lab/limx/MitoEM2021/CODE/HUMAN/rsunet_retrain_297000_v2/"
 
 
     # validation stage: output h5
     # test stage: don't output h5
     for i in range(len(model_id)):  
-        command = "/opt/conda/bin/python {}scripts/main.py --config-file\
-                {}configs/MitoEM/MitoEM-R-BC.yaml\
+        command = "{} {}/scripts/main.py --config-file\
+                {}/configs/MitoEM/MitoEM-R-BC.yaml\
                 --inference\
                 --do_h5\
                 --checkpoint\
-                {}outputs/dataset_output/checkpoint_{:06d}.pth.tar\
+                {}/outputs/dataset_output/checkpoint_{:06d}.pth.tar\
                 --opts\
                 SYSTEM.ROOTDIR\
                 {}\
@@ -114,19 +116,19 @@ if __name__=="__main__":
                 [0,128,128]\
                 INFERENCE.AUG_NUM\
                 0\
-                ".format(root_dir, root_dir, root_dir, model_id[i], root_dir)
+                ".format(which_python, root_dir, root_dir, root_dir, model_id[i], root_dir)
 
         out = subprocess.run(command, shell=True)
         print(command, "\n |-------------| \n", out, "\n |-------------| \n")
 
 
 
-        command = "/opt/conda/bin/python {}connectomics/utils/evaluation/evaluate.py \
+        command = "{} {}/connectomics/utils/evaluation/evaluate.py \
              -gt \
-             /braindat/lab/limx/MitoEM2021/MitoEM-H/MitoEM-H/human_val_gt.h5 \
+             {}/0_human_instance_seg_pred.h5 \
              -p \
-             {}outputs/inference_output/{:06d}_out_100_256_256_aug_0_pad_0.h5 \
-                 -o {}{:06d}".format(root_dir, root_dir, model_id[i], root_dir, model_id[i])
+             {}/outputs/inference_output/{:06d}_out_100_256_256_aug_0_pad_0.h5 \
+                 -o {}{:06d}".format(which_python, root_dir, root_dir, root_dir, model_id[i], root_dir, model_id[i])
 
         out = subprocess.run(command, shell=True)
         print(command, "\n |-------------| \n", out, "\n |-------------| \n")
